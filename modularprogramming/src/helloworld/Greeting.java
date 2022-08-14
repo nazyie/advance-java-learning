9eacfa981ca354672643243506bdc3db64ca0053
Nazyie <nazyie.d@gmail.com>
2022-08-14 13:35:08 +0800

    added module

---
+++ modularprogramming/src/helloworld/Greeting.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Example of the module programming
+ */
+public class Greeting {
+
+    BufferedImage image;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ modularprogramming/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Define the module that we needs
+ */
+module modularprogramming {
+
+    requires java.desktop;
+
+    exports helloworld;
+    exports moduleone.packageone;
+    exports moduleone.packagetwo;
+
+}
